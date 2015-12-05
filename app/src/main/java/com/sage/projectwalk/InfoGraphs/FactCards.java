48dd7910870f057b62d158dadead2b946df8e7d2
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-12-05 19:01:31 +0000

    Added all general facts provided by Daniel.

--- app/src/main/java/com/sage/projectwalk/InfoGraphs/FactCards.java
+++ app/src/main/java/com/sage/projectwalk/InfoGraphs/FactCards.java
@@ -1,7 +1,9 @@
 package com.sage.projectwalk.InfoGraphs;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +11,15 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sage.projectwalk.Data.Country;
 import com.sage.projectwalk.R;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -21,26 +28,23 @@ import java.util.Random;
 
 public class FactCards extends Fragment{
 
-    private TextView mfactLabel;
+    private TextView factBody;
+    private TextView factTitle;
     private Country mCountryOne;
     private Country mCountryTwo;
     Animation slideOutAnimation;
     Animation slideInFromLeftAnim;
     RelativeLayout relativeLayout;
+    ArrayList<String> factTitles;
+    ArrayList<String> facts;
 
-    private String[] Facts = {
-            "Germany's capital is Berlin.",
-            "Germany's estimated population is 81 million.",
-            "German is the official and predominant spoken language in Germany."};
 
-    public String getFact() {
-
-        String fact = "";
+    public void changeFact(){
         //Randomly select a fact
         Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(Facts.length);
-        fact = Facts[randomNumber];
-        return fact;
+        int randomNumber = randomGenerator.nextInt(facts.size());
+        factTitle.setText(factTitles.get(randomNumber));
+        factBody.setText(facts.get(randomNumber));
     }
 
 
@@ -48,7 +52,10 @@ public class FactCards extends Fragment{
 
         View view = inflater.inflate(R.layout.fact_cards, container, false);
 
-        mfactLabel = (TextView) view.findViewById(R.id.textView);
+        factBody = (TextView) view.findViewById(R.id.factBody);
+        factTitle = (TextView) view.findViewById(R.id.factTitle);
+
+        populateFacts();
 
         slideOutAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.slide_out_right);
         slideOutAnimation.setDuration(300);
@@ -60,9 +67,7 @@ public class FactCards extends Fragment{
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                String fact = getFact();
-                //Update the label with a fact from the array
-                mfactLabel.setText(fact);
+                changeFact();
                 relativeLayout.startAnimation(slideInFromLeftAnim);
             }
 
@@ -87,5 +92,40 @@ public class FactCards extends Fragment{
 
     }
 
+    /**
+     * Populates the general facts
+     * Which are stored in the assets folder
+     */
+    public void populateFacts(){
+        factTitles = new ArrayList<>();
+        facts = new ArrayList<>();
+
+        factTitles = getFileAsset("factTitles.txt");
+        facts = getFileAsset("facts.txt");
+        changeFact();
+
     }
+
+    public ArrayList<String> getFileAsset(String fileName){
+        //Load from assets folder
+        AssetManager assetManager = getActivity().getAssets();
+        try{
+            ArrayList<String> fileData = new ArrayList<>();
+            InputStream input = assetManager.open(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(input);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            //Reads contents of file, line by line
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                if(line.length() > 0){
+                    fileData.add(line);
+                }
+            }
+            return fileData;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+}
 
