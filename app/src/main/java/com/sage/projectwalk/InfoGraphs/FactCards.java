96962516e1207e0b445873dbb77e0bed91719671
Tahmidul Islam <k1462015@kcl.ac.uk>
2015-12-05 18:26:24 +0000

    Added animations to fact cards.

--- app/src/main/java/com/sage/projectwalk/InfoGraphs/FactCards.java
+++ app/src/main/java/com/sage/projectwalk/InfoGraphs/FactCards.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -22,7 +24,9 @@ public class FactCards extends Fragment{
     private TextView mfactLabel;
     private Country mCountryOne;
     private Country mCountryTwo;
-
+    Animation slideOutAnimation;
+    Animation slideInFromLeftAnim;
+    RelativeLayout relativeLayout;
 
     private String[] Facts = {
             "Germany's capital is Berlin.",
@@ -46,10 +50,30 @@ public class FactCards extends Fragment{
 
         mfactLabel = (TextView) view.findViewById(R.id.textView);
 
+        slideOutAnimation = AnimationUtils.loadAnimation(getActivity(),R.anim.slide_out_right);
+        slideOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
-        RelativeLayout rl = (RelativeLayout) view.findViewById(R.id.factCardsLayout);
-        rl.setOnClickListener(new View.OnClickListener(){
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                relativeLayout.startAnimation(slideInFromLeftAnim);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        slideInFromLeftAnim = AnimationUtils.loadAnimation(getActivity(),R.anim.slide_in_left);
+
+
+        relativeLayout = (RelativeLayout) view.findViewById(R.id.factCardsLayout);
+        relativeLayout.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
+                relativeLayout.startAnimation(slideOutAnimation);
                 String fact = getFact();
                 //Update the label with a fact from the array
                 mfactLabel.setText(fact);
